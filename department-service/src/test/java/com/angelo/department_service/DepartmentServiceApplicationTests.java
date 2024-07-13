ae1fbf6b11e9e7fb1bd8ff57b19a14f0d20ce633
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-07-12 22:57:47 -0300

    Creating the basis of the 2 microservices

---
+++ department-service/src/test/java/com/angelo/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelo.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
