d81f58fece7315bc9d2eed85b685a1814f2bf878
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-07-14 00:25:13 -0300

    Department creation method completed

---
+++ department-service/src/main/java/com/angelo/department_service/controller/DepartmentController.java
@@ -0,0 +1,27 @@
+package com.angelo.department_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.angelo.department_service.dto.DepartmentDto;
+import com.angelo.department_service.service.DepartmentService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+    
+    private DepartmentService departmentService;
+
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedDepartment);
+    }
+}
