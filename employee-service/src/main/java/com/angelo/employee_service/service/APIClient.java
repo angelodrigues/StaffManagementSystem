b05e1dbe59ba581b5e58b6cafc61ae43cd220fe8
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-07-21 17:05:49 -0300

    Ms communication using Spring Cloud OpenFeign

---
+++ employee-service/src/main/java/com/angelo/employee_service/service/APIClient.java
@@ -0,0 +1,13 @@
+package com.angelo.employee_service.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.angelo.employee_service.dto.DepartmentDto;
+
+@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+public interface APIClient {
+    @GetMapping("api/departments/{department-code}")
+    DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
+}
--- employee-service/src/main/java/com/angelo/employee_service/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/angelo/employee_service/service/impl/EmployeeServiceImpl.java
@@ -2,13 +2,13 @@ package com.angelo.employee_service.service.impl;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.angelo.employee_service.dto.APIResponseDto;
 import com.angelo.employee_service.dto.DepartmentDto;
 import com.angelo.employee_service.dto.EmployeeDto;
 import com.angelo.employee_service.entity.Employee;
 import com.angelo.employee_service.repository.EmployeeRepository;
+import com.angelo.employee_service.service.APIClient;
 import com.angelo.employee_service.service.EmployeeService;
 
 import lombok.AllArgsConstructor;
@@ -21,7 +21,8 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     private ModelMapper modelMapper;
 
-    private WebClient webClient;
+    // private WebClient webClient;
+    private APIClient apiClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -35,16 +36,21 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).get();
-        
+        //---> RestTemplate
         // ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
 
         // DepartmentDto departmentDto = responseEntity.getBody();
-
-        DepartmentDto departmentDto = webClient.get()
-                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
-                .retrieve()
-                .bodyToMono(DepartmentDto.class)
-                .block();
+        //---> RestTemplate
+        
+        //---> WebClient
+        // DepartmentDto departmentDto = webClient.get()
+        //         .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+        //         .retrieve()
+        //         .bodyToMono(DepartmentDto.class)
+        //         .block();
+        //---> WebClient
+        
+        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
 
         EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
 
